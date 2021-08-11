9899dd52bf75984efcc69e521bd76df1089768a8
Zedd1558 <zahidulislam@iut-dhaka.edu>
2021-08-11 18:05:53 +0600

    portforlyouu

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -14,8 +14,8 @@ function initTheme() {
 
   if (savedTheme) {
     // Storage theme
-    // setTheme(savedTheme);
-    setTheme(themes.LIGHT);
+    setTheme(savedTheme);
+    // setTheme(themes.LIGHT);
   } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
     // system theme
     setTheme(themes.DARK);
